a9f7a15d9e53551ec4ab671f6b2d9d59df81eb91
Sean Barry <se@nbarry.co.uk>
2021-05-16 23:15:56 +0100

    refactoring structure

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,17 @@
+mod tokeniser;
+mod parser;
+
 fn main() {
     println!("Hello, world!");
 
-    tokenizer(String::from("(add 2 (subtract 4 2))!"))
+    tokeniser::create_tokens(String::from("(add 2 (subtract 4 2))!"));
+    return;
 }
 
-enum Token {
+pub enum Token {
     Number(String),
-    // String(String),
+    String(String),
     Name(String),
     ParenOpening,
     ParenClosing,
-}
-
-fn tokenizer(input: String) {
-    let mut character_iterable = input.chars().peekable();
-
-    let mut tokens: Vec<Token> = Vec::new();
-
-    while let Some(char) = character_iterable.next() {
-        match char {
-            char if char.is_whitespace() => { println!("whitespace")}
-            '(' => { tokens.push(Token::ParenOpening) }
-            ')' => { tokens.push(Token::ParenClosing)}
-            '0'..='9' => {
-                let mut value = String::new();
-                value.push(char);
-
-                while let Some('0'..='9') = character_iterable.peek() {
-                    value.push(character_iterable.next().unwrap())
-                }
-
-                tokens.push(Token::Number(value))
-            }
-            'a'..='z' => {
-                let mut value = String::new();
-                value.push(char);
-
-                while let Some('a'..='z') = character_iterable.peek() {
-                    value.push(character_iterable.next().unwrap())
-                }
-
-                tokens.push(Token::Name(value))
-            }
-            '"' => {
-                let mut value = String::new();
-
-                while match character_iterable.peek() {
-                    Some(&'"') | None => false,
-                    _ => true,
-                } {
-                    value.push(character_iterable.next().unwrap());
-                }
-
-                 character_iterable.next().unwrap();
-            }
-            _ => { panic!()}
-        }
-    }
 }
